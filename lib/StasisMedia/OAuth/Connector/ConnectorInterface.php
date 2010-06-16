064e479f2eca774257dc6955e76523cc0e58712c
Craig Mason <craig.mason@stasismedia.com>
2010-06-17 00:52:53 +0100

    Add: Connector Interface stub

---
+++ lib/StasisMedia/OAuth/Connector/ConnectorInterface.php
@@ -0,0 +1,18 @@
+<?php
+namespace StasisMedia\OAuth\Connector;
+
+/**
+ * OAuth 1.0 connector interface
+ *
+ * Provides a mechanism to communicate with a Service Provider.
+ * OAuth 1.0 specifies a HTTP interface. We do not enforce HTTP transport
+ * at the interface level
+ *
+ * @author      Craig Mason <craig.mason@stasismedia.com>
+ * @package     OAuth
+ * @subpackage  Connector
+ */
+interface ConnectorInterface
+{
+    
+}
