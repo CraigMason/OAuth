9ecdba5c164ed95f447b6ba44450ebd7ce6dae06
Craig Mason <craig.mason@stasismedia.com>
2010-06-17 00:08:57 +0100

    Add: Client stub

---
+++ lib/StasisMedia/OAuth/Client.php
@@ -0,0 +1,19 @@
+<?php
+namespace StasisMedia\OAuth;
+/* 
+ * OAuth 1.0 client - rfc5849
+ * http://tools.ietf.org/html/rfc5849
+ *
+ * Uses the terminology as specified in rfc5849
+ * 
+ * This library will not initiate any redirection, echo output or otherwise
+ * interfere with the parent application. Redirection is the responsibility
+ * of the application.
+ *
+ * @author      Craig Mason <craig.mason@stasismedia.com>
+ * @package     OAuth
+ */
+class Client
+{
+    
+}
