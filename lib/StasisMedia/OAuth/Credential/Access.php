50df1708efe45d62b4fd25760bdf4188b641f0db
Craig Mason <craig.mason@stasismedia.com>
2010-06-17 00:31:25 +0100

    Add: Access Credential

---
+++ lib/StasisMedia/OAuth/Credential/Access.php
@@ -0,0 +1,61 @@
+<?php
+namespace StasisMedia\OAuth\Credential;
+
+/*
+ * OAuth 1.0 Access credential
+ *
+ * @author      Craig Mason <craig.mason@stasismedia.com>
+ * @package     OAuth
+ * @subpackage  Credential
+ */
+class Access
+{
+    /**
+     * Access token
+     * @var string
+     */
+    private $_token;
+
+    /**
+     * Access secret
+     * @var string
+     */
+    private $_secret;
+
+
+    /**
+     * Access token
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->_token;
+    }
+
+    /**
+     * Access token
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        $this->_token = $token;
+    }
+
+    /**
+     * Access secret
+     * @return string
+     */
+    public function getSecret()
+    {
+        return $this->_secret;
+    }
+
+    /**
+     * Access secret
+     * @param string $secret
+     */
+    public function setSecret($secret)
+    {
+        $this->_secret = $secret;
+    }
+}
