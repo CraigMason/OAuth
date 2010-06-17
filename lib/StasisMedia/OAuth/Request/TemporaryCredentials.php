0716ee2fe15e5f5b837ed5f320dc88c9a1c74e96
Craig Mason <craig.mason@stasismedia.com>
2010-06-17 02:45:36 +0100

    Add: TemporaryCredentials request stub

---
+++ lib/StasisMedia/OAuth/Request/TemporaryCredentials.php
@@ -0,0 +1,37 @@
+<?php
+namespace StasisMedia\OAuth\Request;
+
+use StasisMedia\OAuth\Credential\Consumer;
+/**
+ * OAuth 1.0 Temporary Credentials request
+ * http://tools.ietf.org/html/rfc5849#section-2.1
+ *
+ * Used for requesting temporary credentials. Stage 1 in the workflow.
+ *
+ *
+ * @author      Craig Mason <craig.mason@stasismedia.com>
+ * @package     OAuth
+ * @subpackage  Request
+ */
+class TemporaryCredentials extends Request implements RequestInterface
+{
+    /**
+     * A Consumer credential containing key and secret
+     * @var Consumer
+     */
+    private $_consumerCredentials;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->addRequiredOAuthParameters(array(
+            'oauth_callback'
+        ));
+    }
+
+    public function setConsumerCredentials(Consumer $consumerCredentials)
+    {
+        $this->_consumerCredentials = $consumerCredentials;
+    }
+}
