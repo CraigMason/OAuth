35e1b869943850227bb2a9fc58dfde0a06ba8f9c
Craig Mason <craig.mason@stasismedia.com>
2010-06-17 01:07:26 +0100

    Add: Curl Connector stub

---
+++ lib/StasisMedia/OAuth/Connector/HTTP/Curl.php
@@ -0,0 +1,17 @@
+<?php
+namespace StasisMedia\OAuth\Connector\HTTP;
+
+use StasisMedia\OAuth\Connector;
+
+/*
+ * OAuth 1.0 HTTP Curl connector
+ *
+ * CURL connector for communicating with a Service Provider using HTTP
+ *
+ * @author      Craig Mason <craig.mason@stasismedia.com>
+ * @package     OAuth
+ */
+class Curl implements Connector\ConnectorInterface
+{
+    
+}
