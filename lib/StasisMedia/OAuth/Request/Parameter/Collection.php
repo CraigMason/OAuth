6b551827a6a2029ad1aa775b9ff0fd5e648b386a
Craig Mason <craig.mason@stasismedia.com>
2010-06-20 12:47:59 +0100

    Add Collection implementation

--- lib/StasisMedia/OAuth/Request/Parameter/Collection.php
+++ lib/StasisMedia/OAuth/Request/Parameter/Collection.php
@@ -12,9 +12,67 @@ class Collection
 {
     private $_parameters = array();
 
-
-    public function add(Parameter $parameter)
+    public function add($name, $values)
     {
-        $this->_parameters[] = $parameter;
+        $name = (string) $name;
+
+        $values = (array) $values;
+
+        // Create Parameter if it does not exist
+        if(!$this->exists($name))
+        {
+            $this->_parameters[$name] = new Parameter($name, $values);
+        }
+        else {
+            $this->get($name)->addValues($values);
+        }
+    }
+
+    /**
+     *
+     * @param string $name
+     * @return Parameter
+     */
+    public function get($name)
+    {
+        return $this->exists($name) ? $this->_parameters[$name] : null;
+    }
+
+    public function getAll()
+    {
+        return $this->_parameters;
+    }
+
+    public function exists($name)
+    {
+        return array_key_exists($name, $this->_parameters);
+    }
+
+    private function sort()
+    {
+        usort($this->_parameters, function($a, $b){
+            /* @var $a Parameter */
+            /* @var $b Parameter */
+            return strcmp(
+                $a->getName()->getPercentEncoded(),
+                $b->getName()->getPercentEncoded()
+            );
+        });
+    }
+
+    /**
+     * @return string OAuth normalized string
+     */
+    public function getNormalized()
+    {
+        $this->sort();
+        $pairs = array();
+        foreach($this->_parameters as $parameter)
+        {
+            /* @var $parameter Parameter */
+            $pairs[] = $parameter->getNormalized();
+        }
+
+        return implode('&', $pairs);
     }
 }
