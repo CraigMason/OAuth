5c4cee02a89ed10ac3a79f32abbb33fb4545dbe7
Craig Mason <craig.mason@stasismedia.com>
2010-06-22 22:30:05 +0100

    Add missing MockRequest for Unit Tests

--- test/StasisMedia/OAuth/Request/MockRequest.php
+++ test/StasisMedia/OAuth/Request/MockRequest.php
@@ -3,8 +3,8 @@ namespace StasisMedia\OAuth\Request;
 
 class MockRequest extends Request implements RequestInterface
 {
-    public function parseResponse(\StasisMedia\OAuth\Response\HTTP $response)
-    {
-    }
+    public function parseResponse(\StasisMedia\OAuth\Response\HTTP $response){}
+
+    public function prepare(){}
 
 }
