fe1147e5d1b24688c96aa14a4bb2b9167f8fb58e
Craig Mason <craig.mason@stasismedia.com>
2010-06-22 22:07:53 +0100

    Add TemporaryAccess credential
    The spec explains that the token/secret are temporary until we have been
    granted full-access tokens. TemporaryAccess is just a blank extension
    to denote this difference

---
+++ lib/StasisMedia/OAuth/Credential/TemporaryAccess.php
@@ -0,0 +1,12 @@
+<?php
+namespace StasisMedia\OAuth\Credential;
+
+/**
+ * OAuth 1.0 Tamporary Access credential
+ *
+ * @author      Craig Mason <craig.mason@stasismedia.com>
+ * @package     OAuth
+ * @subpackage  Credential
+ */
+class TemporaryAccess extends Access
+{}
