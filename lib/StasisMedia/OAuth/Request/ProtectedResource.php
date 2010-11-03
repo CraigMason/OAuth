0dccf4024fd5a4a5c1e806532cae48c69002542c
Craig Mason <craig.mason@stasismedia.com>
2010-11-03 08:12:23 +0000

    Rename to singular

--- lib/StasisMedia/OAuth/Request/ProtectedResource.php
+++ lib/StasisMedia/OAuth/Request/ProtectedResource.php
@@ -4,7 +4,7 @@ namespace StasisMedia\OAuth\Request;
 use StasisMedia\OAuth\Parameter;
 use StasisMedia\OAuth;
 
-class ProtectedResources extends Request
+class ProtectedResource extends Request
 {
     public function setAccessCredential(OAuth\Credential\Access $credential)
     {
